6604221bd122191f07528b88535326c1af0a7602
Geomar Perales Apaico <per3app@gmail.com>
2021-03-06 08:39:15 -0500

    Add files via upload

---
+++ R/estaciones.Pe.R
@@ -0,0 +1,13 @@
+#' Estaciones del Peru (SENAMHI).
+#'
+#' Un conjunto de datos de 22 estaciones del Perú (SENAMHI)
+#'.
+#' @format un marco de datos con 22 filas de información de estaciones.
+#' \describe{
+#'   \item{Estacion}{Nombre de la estación}
+#'   \item{Lat}{Latitud}
+#'   \item{Lon}{Longitud}
+#'   \item{Alt}{Altitud, en msnm.}
+#'   ...
+#' }
+"estaciones.Pe"
