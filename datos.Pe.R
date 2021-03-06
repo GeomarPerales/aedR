97071fbdc24da00f46d2a46cf45c70fe1831a341
Geomar Perales Apaico <per3app@gmail.com>
2021-03-06 08:37:48 -0500

    Add files via upload

---
+++ datos.Pe.R
@@ -0,0 +1,11 @@
+#' Datos históricos de estaciones del Perú (SENAMHI).
+#'
+#' Un conjunto de datos de 22 series históricas de estaciones del Perú (SENAMHI).
+#'
+#' @format un marco de datos con 22 series de tiempo mensuales. La primera fila
+#' corresponde a la fecha, las siguientes corresponden a las series de tiempo mensuales.
+#' \describe{
+#'   \item{Fecha}{el registro de fechas es mensual, desde 01-01-1964 hasta 31-12-2016}
+#'   ...
+#' }
+"datos.Pe"
